f1a092e091907ec70c12b8befdbf8f08d7014752
GoRoSfan <olsngrs@gmail.com>
2020-01-29 21:04:52 +0200

    add Menu component

--- src/components/MainTemplate.vue
+++ src/components/MainTemplate.vue
@@ -2,53 +2,27 @@
     <div>
         <header class="page-header">
             <div class="container">
-                <div class="menu-item">
-                    <h3 @click="visible_menu = !visible_menu">Contacts</h3>
-                    <div class="sub-menu" :style="visible_menu ? visibleSet : hiddenSet">
-                        <dl>
-                            <dt>Phone:</dt>
-                            <dd>
-                                <a href="tel:+380993907922" class="header-phone">+38 099 390 79 22</a>
-                            </dd>
-
-                            <dt>E-mail:</dt>
-                            <dd>
-                                <a href="mailto:olegsinilo126@gmail.com" class="header-email">olegsinilo126@gmail.com</a>
-                            </dd>
-
-                            <dt>Telegram:</dt>
-                            <dd><a href="https://t.me/Sinilo_Oleg" class="header-telegram">Oleg OLEG oleg</a></dd>
-                        </dl>
-                    </div>
-                </div>
+                <Menu></Menu>
             </div>
         </header>
         <slot id="content"></slot>
         <footer class="page-footer">
             <div class="container">
                 Буду рад Вам помочь
-                <a class="footer-email" href="mailto:olegsinilo126@gmail.com">olegsinilo126@gmail.com</a>
-                <a class="footer-phone" href="tel:+380993907922">+38 099 390 79 22</a>
+                <a class="email" href="mailto:olegsinilo126@gmail.com">olegsinilo126@gmail.com</a>
+                <a class="phone" href="tel:+380993907922">+38 099 390 79 22</a>
             </div>
         </footer>
     </div>
 </template>
 
 <script>
+    import Menu from "./Menu"
+
     export default {
         name: "MainTemplate",
-        data: () => {
-            return {
-                visible_menu: false,
-                visibleSet: {
-                    opacity: '1',
-                    visibility: 'visible'
-                },
-                hiddenSet: {
-                    opacity: '0',
-                    visibility: 'hidden'
-                }
-            }
+        components: {
+            Menu,
         },
     }
 </script>
@@ -58,39 +32,6 @@
         max-width: 1408px;
         margin: 0 auto;
         padding: 0 16px;
-    }
-</style>
-
-<style scoped>
-    .page-header {
-        margin: 0;
-        padding: 20px 0;
-    }
-
-    .page-header .menu-item {
-        position: relative;
-
-        margin: 5px;
-
-        z-index: 10;
-    }
-
-    .page-header h3 {
-        margin: 0;
-    }
-
-    .page-header .sub-menu {
-        visibility: hidden;
-        opacity: 0;
-
-        position: absolute;
-        top: 100%;
-        left: 0;
-
-        -webkit-transition: 0.5s ease-in-out;
-        -moz-transition: 0.5s ease-in-out;
-        -o-transition: 0.5s ease-in-out;
-        transition: 0.5s ease-in-out;
     }
 
     .page-header a,
@@ -130,35 +71,37 @@
         width: 100%;
     }
 
-    .header-email,
-    .footer-email {
+    .page-header a:active,
+    .page-footer a:active {
+        opacity: 0.5;
+    }
+
+    .email {
         padding-left: 28px;
         background-image: url("../assets/img/icon-email.svg");
     }
 
-    .header-email:focus,
-    .header-email:hover,
-    .footer-email:focus,
-    .footer-email:hover {
+    .email:focus,
+    .email:hover {
         background-image: url("../assets/img/icon-email-alt.svg");
     }
 
-    .header-phone,
-    .footer-phone {
+    .phone {
         padding-left: 21px;
         background-image: url("../assets/img/icon-phone.svg");
     }
 
-    .header-phone:focus,
-    .header-phone:hover,
-    .footer-phone:focus,
-    .footer-phone:hover {
+    .phone:focus,
+    .phone:hover {
         background-image: url("../assets/img/icon-phone-alt.svg");
     }
 
-    .page-header a:active,
-    .page-footer a:active {
-        opacity: 0.5;
+</style>
+
+<style scoped>
+    .page-header {
+        margin: 0;
+        padding: 20px 0;
     }
 
     .page-footer {
---
+++ src/components/Menu.vue
@@ -0,0 +1,60 @@
+<template>
+    <nav class="menu">
+        <div class="menu-item">
+            <h3 @click="visible_menu = !visible_menu">Contacts</h3>
+            <div :class="{'visible-set': visible_menu}" class="sub-menu" >
+                <dl>
+                    <dt>Phone:</dt>
+                    <dd>
+                        <a href="tel:+380993907922" class="header-phone">+38 099 390 79 22</a>
+                    </dd>
+
+                    <dt>E-mail:</dt>
+                    <dd>
+                        <a href="mailto:olegsinilo126@gmail.com" class="header-email">olegsinilo126@gmail.com</a>
+                    </dd>
+
+                    <dt>Telegram:</dt>
+                    <dd><a href="https://t.me/Sinilo_Oleg" class="header-telegram">Oleg OLEG oleg</a></dd>
+                </dl>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: "Menu",
+        data: () => {
+            return {
+                visible_menu: false,
+            }
+        },
+    }
+</script>
+
+<style scoped>
+    h3{
+        margin: 0;
+    }
+
+    .menu-item {
+        position: relative;
+    }
+
+    .sub-menu {
+        visibility: hidden;
+        opacity: 0;
+
+        position: absolute;
+        top: 100%;
+        left: 0;
+
+        transition: 0.5s ease-in-out;
+    }
+
+    .visible-set{
+        opacity: 1;
+        visibility: visible;
+    }
+</style>
